334c0715699bf47eb2ad6bfda0dcfba24e993029
Almaz Ahmad <almazahmad@pc-197-235.customer.ask4.lan>
2020-03-25 12:52:40 +0000

    Add static build

---
+++ build/precache-manifest.5c3cf723519f814d115d62486512b948.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a177e4a20ebabd2bd4109eaf686c31f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8289a6546d3cb95549ab",
+    "url": "/static/css/main.8b04f90c.chunk.css"
+  },
+  {
+    "revision": "9febd95578f74c0d556c",
+    "url": "/static/js/2.ed99db2c.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.ed99db2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8289a6546d3cb95549ab",
+    "url": "/static/js/main.03dfb0ba.chunk.js"
+  },
+  {
+    "revision": "c7ca1642fec3dd4859a3",
+    "url": "/static/js/runtime-main.9b080a5c.js"
+  },
+  {
+    "revision": "d9cf23aa6ea50da8fd756c041f11110e",
+    "url": "/static/media/Arabic.d9cf23aa.jpg"
+  },
+  {
+    "revision": "ae5128552b30e8593d8e8b20d214fb4a",
+    "url": "/static/media/French.ae512855.jpg"
+  },
+  {
+    "revision": "fabdf0c3a577a328fb7e094a27ed16e9",
+    "url": "/static/media/German.fabdf0c3.jpg"
+  },
+  {
+    "revision": "fbe469f1e1d8dc208294020037cda58d",
+    "url": "/static/media/Italian.fbe469f1.jpg"
+  },
+  {
+    "revision": "b4c80b81716c0dc4ad645567b983f779",
+    "url": "/static/media/error.b4c80b81.jpg"
+  },
+  {
+    "revision": "54061e4354039b5b1033c4e1cdfb50ea",
+    "url": "/static/media/landing1.54061e43.jpg"
+  },
+  {
+    "revision": "e99930477409436e8c7ed4bf00975001",
+    "url": "/static/media/landing3.e9993047.jpg"
+  },
+  {
+    "revision": "0ff7d8951e665619a2dc22d26938d854",
+    "url": "/static/media/notesLanding.0ff7d895.jpg"
+  },
+  {
+    "revision": "dc2060ae8c6acc1a54375db1d5753824",
+    "url": "/static/media/spanish.dc2060ae.jpg"
+  }
+]);
